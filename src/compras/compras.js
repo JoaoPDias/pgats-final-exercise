92612733490dd03e6d0bd33c22e894724623e1d2
Joao Dias <joao.dias@ingenious.build>
2024-05-02 00:20:11 -0300

    fix: using the finding correctly

--- src/compras/compras.js
+++ src/compras/compras.js
@@ -8,15 +8,17 @@ const calcularTotal = (ferramentas, comprar) => {
     throw new Error("Ambas as listas precisam ter ao menos um item.");
   }
   let total = 0;
+  let encontradas = [];
   ferramentas.forEach((ferramenta) => {
     if (comprar.includes(ferramenta.nome)) {
+      encontradas.push(ferramenta.nome);
       total += ferramenta.preco;
     }
   });
   if (total === 0) {
     throw new Error("Nenhuma ferramenta desejada encontrada.");
   }
-  return `O valor a pagar pelas ferramentas (${comprar.join(
+  return `O valor a pagar pelas ferramentas (${encontradas.join(
     ", "
   )}) é R$ ${total.toFixed(2)}`;
 };
